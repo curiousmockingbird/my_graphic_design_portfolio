3a942573cd974960e1a1a0f1ea6ee8be7366cf75
Harold Mesa <harold.mesa@icloud.com>
2023-11-06 15:51:53 -0600

    Add revalidate constant in utils/getCloudinaryResources.ts.

--- src/app/utils/getCloudinaryResources.ts
+++ src/app/utils/getCloudinaryResources.ts
@@ -1,6 +1,8 @@
 import {v2 as cloudinary} from 'cloudinary'
 // import {cache} from 'react'
 
+export const revalidate = 3600 
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
