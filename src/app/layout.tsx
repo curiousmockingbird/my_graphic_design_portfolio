5247b40f572a18c26f95781eb76c4c972f0576dd
Harold Mesa <harold.mesa@icloud.com>
2023-11-17 16:36:05 -0600

    Adding @vercel/analitics/react.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import TranStackProvider from './components/providers/TanStackProvider'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <TranStackProvider>
         {children}
+        <Analytics />
         </TranStackProvider>
         </body>
     </html>
