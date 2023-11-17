a6195937d2bb787fe56f89ef96e07bbc901b2c58
Harold Mesa <harold.mesa@icloud.com>
2023-11-17 16:44:42 -0600

    Modified app/layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,8 @@ export default function RootLayout({
       <body className={inter.className}>
         <TranStackProvider>
         {children}
-        <Analytics />
         </TranStackProvider>
+        <Analytics />
         </body>
     </html>
   )
