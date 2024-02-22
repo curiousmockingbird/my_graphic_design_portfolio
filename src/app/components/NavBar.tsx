be2a65c875434ea44c944bc643a26e3effae3f1c
Harold Mesa <harold.mesa@icloud.com>
2024-02-22 17:28:38 -0600

    Add condition to rendering of <NavBar/>

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,40 +1,107 @@
+import React, { useState } from 'react';
 import Link from 'next/link';
+import { AppBar, Toolbar, IconButton, Menu, MenuItem, useMediaQuery, useTheme } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import ContactMailOutlinedIcon from '@mui/icons-material/ContactMailOutlined';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function Navbar() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const handleMenu = (event:any) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <div className="h-full grid grid-cols-5">
-      <div className='col-span-2 flex justify-center items-center'>
-        <div className='text-white'>
-          <ContactMailOutlinedIcon className="nav-items" />
-          harolDesigner.art
+    <>
+      {isMobile ? (
+        <AppBar position="static">
+          <Toolbar>
+            <div className='flex justify-start items-center'>
+              <ContactMailOutlinedIcon />
+              <p>harolDesigner.art</p>
+            </div>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ ml: 2 }}
+              onClick={handleMenu}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={open}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>
+                <Link href="/about">About</Link>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <Link href="/contact">Contact</Link>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <a href="https://www.instagram.com/harold_designer/" target="_blank" rel="noopener noreferrer">Instagram</a>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <a href="https://www.linkedin.com/in/haroldmesa93/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+              </MenuItem>
+            </Menu>
+          </Toolbar>
+        </AppBar>
+      ) : (
+        <div className="h-full grid grid-cols-5">
+          <div className='col-span-2 flex justify-center items-center'>
+            <div className='text-white'>
+              <ContactMailOutlinedIcon className="nav-items" />
+              harolDesigner.art
+            </div>
+          </div>
+          <div className='flex justify-center items-center nav-menu'>
+            <Link href="/about"
+              className='text-white hover:text-tahiti'>
+              <InfoOutlinedIcon className="nav-items" />
+              about
+            </Link>
+          </div>
+          <div className='flex justify-center items-center nav-menu'>
+            <Link href="/contact"
+              className='text-white hover:text-tahiti'>
+              <ContactMailOutlinedIcon className="nav-items" />
+              contact
+            </Link>
+          </div>
+          <div className='flex justify-center items-center nav-menu'>
+            <a href="https://www.instagram.com/harold_designer/" target="_blank" rel="noopener noreferrer" className='text-white hover:text-tahiti'>
+              <InstagramIcon sx={{ fontSize: 40 }} />
+            </a>
+            <a href="https://www.linkedin.com/in/haroldmesa93/" target="_blank" rel="noopener noreferrer">
+              <LinkedInIcon sx={{ fontSize: 40 }} />
+            </a>
+          </div>
         </div>
-      </div>
-      <div className='flex justify-center items-center nav-menu'>
-        <Link href="/about"
-          className='text-white  hover:text-tahiti'>
-          <InfoOutlinedIcon className="nav-items" />
-          about
-        </Link>
-      </div>
-      <div className='flex justify-center items-center nav-menu'>
-        <Link href="/contact"
-          className='text-white  hover:text-tahiti'>
-          <ContactMailOutlinedIcon className="nav-items" />
-          contact
-        </Link>
-      </div>
-      <div className='flex justify-center items-center nav-menu'>
-        <a href="https://www.instagram.com/harold_designer/" target="_blank" rel="noopener noreferrer" className='text-white  hover:text-tahiti'>
-          <InstagramIcon sx={{ fontSize: 40 }} />
-        </a>
-        <a href="https://www.linkedin.com/in/haroldmesa93/" target="_blank" rel="noopener noreferrer">
-          <LinkedInIcon sx={{ fontSize: 40 }} />
-        </a>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
